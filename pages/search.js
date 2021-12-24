26511975eede2bfe0d88f8842bc3380acf226113
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-12-24 17:12:55 +0700

    Fix repeating import

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ import Property from "../components/Property";
 import SearchFilters from "../components/SearchFilters";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import noresult from "../assets/images/noresult.svg";
-import { baseUrl } from "../utils/fetchApi";
+
 const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
